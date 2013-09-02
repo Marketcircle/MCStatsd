8a4ec0a2b5ab5c53cff4a6691dfcacaf1c542736
Thomas Bartelmess <tbartelmess@marketcircle.com>
2013-09-01 20:49:44 -0400

    Initial Commit

---
+++ MCStatsd/MCStatsd.h
@@ -0,0 +1,13 @@
+//
+//  MCStatsd.h
+//  MCStatsd
+//
+//  Created by Thomas Bartelmess on 2013-09-01.
+//  Copyright (c) 2013 Marketcircle Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCStatsd : NSObject
+
+@end
---
+++ MCStatsd/MCStatsd.m
@@ -0,0 +1,13 @@
+//
+//  MCStatsd.m
+//  MCStatsd
+//
+//  Created by Thomas Bartelmess on 2013-09-01.
+//  Copyright (c) 2013 Marketcircle Inc. All rights reserved.
+//
+
+#import "MCStatsd.h"
+
+@implementation MCStatsd
+
+@end
